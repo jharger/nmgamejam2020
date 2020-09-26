22c3a819d1d7c641e1e9516fc9139f06602ff87c
John Harger <john.harger@gmail.com>
2020-09-26 14:40:50 -0500

    Added level restarting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GodComplex.Utility;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager> {
+    private void Awake() {
+        StartGame();
+    }
+
+    private void Update() {
+        // TODO remove
+        if (Input.GetKeyDown(KeyCode.R)) {
+            RestartLevel();
+        }
+    }
+
+    public void StartGame() {
+        if (SceneManager.sceneCount < 2) {
+            SceneManager.LoadScene("MovementTest", LoadSceneMode.Additive);
+        }
+    }
+
+    public void RestartLevel() {
+        SceneManager.UnloadSceneAsync("MovementTest");
+        SceneManager.LoadScene("MovementTest", LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GodComplex.Utility
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (!_instance)
+                {
+                    _instance = (T) FindObjectOfType(typeof(T));
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
