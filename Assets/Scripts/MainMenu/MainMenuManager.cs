322a81e6b57cf9648401898e21ad610514dd6291
bgonz12 <bgonz12@hotmail.com>
2020-09-27 00:14:56 -0400

    Implement first pass at main menu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public string playSceneName;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(playSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
